8bd2fa8d391211bd420abb7daddf9cc8d4d49fa0
PoivronJaune <poivronjaune@gmail.com>
2024-11-24 12:31:20 -0500

    New TradingUniverse class and test utilities

---
+++ src/bhtp/universe.py
@@ -0,0 +1,83 @@
+import pandas as pd
+from bhtp.github import Github
+
+class TradingUniverse:
+    """Trading Universe Class contains all trading information to simulate a trading environnement 
+       with price data including multiple timeframes.
+    """
+
+    def __init__(self):
+        self.df_1min = None
+        self.df_5min = None
+        self.df_15min = None
+        self.df_1hr = None
+        self.df_4hr = None
+        self.df_1day = None
+        self.df_1wk = None
+        self.df_1month = None
+
+    def timeframe(self, timeframe: str) -> pd.DataFrame:
+        """
+        If data for 1 minute time frame exists, aggregate data to another timeframe.
+        requires self.df_1min to contain valid data
+
+        Parameters:
+        ============
+        timeframe : str
+            Target timeframe ['5min', '15min', '1h','4h', '1D', '1W', '1ME'] # ME = Month End
+
+        """
+        if self.df_1min is None:
+            raise ValueError('No minute data available to aggregate.')
+        if timeframe not in ['5min', '15min', '1h','4h', '1D', '1W', '1ME']:
+            raise ValueError("Invalid timeframe target, Use : ['5min', '15min', '1H','4H', '1D', '1W', '1ME']")
+        
+        new_timeframe = (
+            self.df_1min.groupby('Symbol')
+            .resample(timeframe)      # new timeframe frequency
+            .agg({
+                'Open': 'first',      # First open in the interval
+                'High': 'max',        # Maximum high in the interval
+                'Low': 'min',         # Minimum low in the interval
+                'Close': 'last',      # Last close in the interval
+                'Volume': 'sum'       # Total volume in the interval
+            })
+            #.drop(columns='Symbol')   # Remove Symbol from multi-level index
+            .reset_index()            # Reset index to make it a standard DataFrame
+        )
+        new_timeframe = new_timeframe[['Datetime','Symbol','Open','High','Low','Close','Volume']]
+        return new_timeframe  
+
+    def insert_data(self, df_ohlcv: pd.DataFrame) -> None:
+        """
+        Inserts minute price data in the trading universe. Dataframe should contain the following columns
+        [Datetime, Symbol, Open, High, Low, Close, Volume]. 
+        Use the Github class to load online data from Github repositories. 
+
+        Parameters:
+        ============
+        df_ohlcv : Pandas Dataframe
+            Dataframe containing minute price data [Datetime, Symbol, Open, High, Low, Close, Volume]
+
+        """
+        # Reorganise minute data for Trading Universe structure, sorted, index and columns
+        self.df_1min = df_ohlcv
+        self.df_1min = self.df_1min.sort_values(by=['Symbol', 'Datetime'])
+        self.df_1min['Datetime'] = pd.to_datetime(self.df_1min['Datetime'])
+        self.df_1min = self.df_1min[['Datetime','Symbol','Open','High','Low','Close','Volume']]
+        self.df_1min.set_index('Datetime', inplace=True)
+
+        self.df_5min = self.timeframe('5min')
+        self.df_15min = self.timeframe('15min')
+        self.df_1hr = self.timeframe('1h')
+        self.df_4hr = self.timeframe('4h')
+        self.df_1day = self.timeframe('1D')
+        self.df_1wk = self.timeframe('1W')
+        self.df_1month = self.timeframe('1ME')      
+
+    def calculate_indicators(self) -> None:
+        """Using data loaded in Universe (1min, 5min, 15min, 1h, 4h, 1d, 1w, 1month) calculate a bunch of indicators.
+        Append all indicators in Universe data as new DataFrame columns.
+        """
+        pass
+
---
+++ tests/test_universe.py
@@ -0,0 +1,71 @@
+import time
+from tkinter import N
+import pandas as pd
+from numpy import true_divide
+import pytest
+from bhtp.github import Github
+from bhtp.universe import TradingUniverse
+
+@pytest.fixture(scope="session")
+def tu1():
+    # Default trading universe
+    return TradingUniverse()
+
+@pytest.fixture(scope="session")
+def g_data():
+    data_df = pd.read_csv('tests/test_data.csv')
+
+    ####
+    # Remove comments to test directly from github repo.
+    ######
+    #g1 = Github()
+    #data_df = pd.DataFrame()
+    #files = ['NASDAQ-CC0-2023-04-03.csv', 'NASDAQ-BM0-2023-04-22.csv', 'NASDAQ-TE0-2023-04-20.csv'] 
+    #max_retries = 5
+    #for csv_file in files:
+    #    try_to_load = True
+    #    loading_attempt = 1
+    #    while try_to_load:
+    #        try:
+    #            data = g1.load_ohlcv_from_file(csv_file)
+    #            try_to_load = False
+    #        except ConnectionError as e:
+    #            if loading_attempt > max_retries:
+    #                try_to_load = False
+    #                raise ConnectionError(f'{max_retries} retries in test to load github\n{e}')
+    #            loading_attempt += 1
+    #            time.sleep(2)
+    #
+    #    data_df = pd.concat([data_df, data], ignore_index=True)
+    ##    data_df.to_csv('test_data.csv', index=False)
+    return data_df
+
+
+def test_init_setup(tu1):
+    tu = tu1
+    assert tu.df_1min is None
+    assert tu.df_5min is None
+    assert tu.df_15min is None
+    assert tu.df_1hr is None
+    assert tu.df_4hr is None
+    assert tu.df_1day is None
+    assert tu.df_1wk is None
+    assert tu.df_1month is None
+
+def test_insert_data(tu1, g_data):
+    print(f'\n*********\n Loading test data may take 30-60 seconds\n*********\n')
+    assert tu1 is not None
+    assert tu1.df_1min is None
+    assert g_data is not None
+    print(g_data)
+    tu1.insert_data(g_data)
+    assert tu1.df_1min is not None
+    assert tu1.df_5min is not None
+    assert tu1.df_15min is not None
+    assert tu1.df_1hr is not None
+    assert tu1.df_4hr is not None
+    assert tu1.df_1day is not None
+    assert tu1.df_1wk is not None
+    assert tu1.df_1month is not None
+    
+    
